08894f36f9864f1fec36113ce51300479c6a1df7
Divya Narayan <narayanadivya32@gmail.com>
2026-06-22 15:19:57 +0530

    Bulk sync: 7 submissions

---
+++ Data Structures & Algorithms/anagram-groups/submission-0.java
@@ -0,0 +1,26 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        
+
+                     Map<String, List<String>> map = new HashMap<>();
+
+        for (String str : strs) {
+
+            int[] count = new int[26];
+
+            for (char ch : str.toCharArray()) {
+                count[ch - 'a']++;
+            }
+
+            StringBuilder key = new StringBuilder();
+
+            for (int num : count) {
+                key.append("#").append(num);
+            }
+
+            map.computeIfAbsent(key.toString(), k -> new ArrayList<>()).add(str);
+        }
+
+        return new ArrayList<>(map.values());
+    }
+}
---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-0.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int[] topKFrequent(int[] nums, int k) {
+        
+      HashMap<Integer,Integer> count=new HashMap<>();
+
+      for(int num:nums){
+        count.put(num,count.getOrDefault(num,0)+1);
+      }
+
+      PriorityQueue<Integer> heap=new PriorityQueue<>((a,b)-> count.get(a)-count.get(b));
+
+      for(int num:count.keySet()){
+
+        heap.offer(num);
+
+        if(heap.size()>k){
+            heap.poll();
+        }
+      }
+
+      int[] result=new int[k];
+
+      for(int i=k-1;i>=0;i--){
+        result[i]=heap.poll();
+      }
+      return result;
+    }
+}
